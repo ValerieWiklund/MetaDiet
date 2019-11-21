d953323edb8889545d75ba63affd0610e4fbfebc
Valerie Wiklund <vkwiklund@gmail.com>
2019-11-21 15:26:19 -0700

    Created starting table

--- db-setup.sql
+++ db-setup.sql
@@ -1,62 +1,57 @@
--- CREATE TABLE users
--- (
---   id VARCHAR(255) NOT NULL,
---   username VARCHAR(255) NOT NULL,
---   email VARCHAR(255) NOT NULL,
---   hash VARCHAR(255) NOT NULL,
---   PRIMARY KEY (id),
---   UNIQUE
---   KEY email
---   (email)
--- );
+CREATE TABLE users
+(
+  id VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  hash VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE
+  KEY email
+  (email)
+);
 
---   CREATE TABLE vaults
---   (
---     id int NOT NULL
---     AUTO_INCREMENT,
---     name VARCHAR
---     (255) NOT NULL,
---     description VARCHAR
---     (255) NOT NULL,
---     userId VARCHAR
---     (255),
---     INDEX userId
---     (userId),
---     FOREIGN KEY
---     (userId)
---         REFERENCES users
---     (id)
---         ON
---     DELETE CASCADE,  
---     PRIMARY KEY (id)
---     );
+  --   CREATE TABLE vaults
+  --   (
+  --     id int NOT NULL
+  --     AUTO_INCREMENT,
+  --     name VARCHAR
+  --     (255) NOT NULL,
+  --     description VARCHAR
+  --     (255) NOT NULL,
+  --     userId VARCHAR
+  --     (255),
+  --     INDEX userId
+  --     (userId),
+  --     FOREIGN KEY
+  --     (userId)
+  --         REFERENCES users
+  --     (id)
+  --         ON
+  --     DELETE CASCADE,  
+  --     PRIMARY KEY (id)
+  --     );
 
---     CREATE TABLE keeps
---     (
---       id int NOT NULL
---       AUTO_INCREMENT,
---     name VARCHAR
---       (255) NOT NULL,
---     description VARCHAR
---       (255) NOT NULL,
---     userId VARCHAR
---       (255),
---     img VARCHAR
---       (255),
---     isPrivate TINYINT,
---     views INT DEFAULT 0,
---     shares INT DEFAULT 0,
---     keeps INT DEFAULT 0,
---     INDEX userId
---       (userId),
---     FOREIGN KEY
---       (userId)
---         REFERENCES users
---       (id)
---         ON
---       DELETE CASCADE,  
---     PRIMARY KEY (id)
---       );
+  CREATE TABLE foods
+  (
+    id int NOT NULL
+    AUTO_INCREMENT,
+    name VARCHAR
+    (255) NOT NULL,
+    description VARCHAR
+    (255) NOT NULL,
+    category VARCHAR
+    (255) NOT NULL,
+    phase1 TINYINT NOT NULL,
+    phase2 TINYINT NOT NULL,
+    phase3 TINYINT NOT NULL,
+    servingnum INT NOT NULL,
+    sizedescription VARCHAR
+    (255) NOT NULL,
+    INDEX
+    (phase1, phase2, phase3),         
+    PRIMARY KEY
+    (id)
+    );
 
 --       CREATE TABLE vaultkeeps
 --       (

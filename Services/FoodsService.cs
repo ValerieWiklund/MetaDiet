fa06b8899c71401d4b07e4e116cdf849b0973181
Valerie Wiklund <vkwiklund@gmail.com>
2019-12-04 20:22:12 -0700

    started service

--- Services/FoodsService.cs
+++ Services/FoodsService.cs
@@ -1,7 +1,13 @@
+using MetaDiet.Repositories;
+
 namespace MetaDiet.Services
 {
-    public class FoodsService
+  public class FoodsService
+  {
+    private readonly FoodsRepository _repo;
+    public FoodsService(FoodsRepository repo)
     {
-        
+      _repo = repo;
     }
+  }
 }

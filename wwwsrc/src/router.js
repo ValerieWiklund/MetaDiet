c99d3c3031202045fcd4d80be0bde9ef33b19c2e
Valerie Wiklund <vkwiklund@gmail.com>
2020-01-30 10:29:20 -0700

    oops - imports for router

--- wwwsrc/src/router.js
+++ wwwsrc/src/router.js
@@ -1,20 +1,28 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+// @ts-ignore
 Vue.use(Router);
 
+// @ts-ignore
 export default new Router({
   routes: [
     {
       path: "/",
       name: "home",
+      // @ts-ignore
       component: Home
     },
     {
       path: "/login",
       name: "login",
+      // @ts-ignore
       component: Login
     },
     {
       path: "/profile/userId",
       name: "profile",
+      // @ts-ignore
       component: Profile
     }
   ]

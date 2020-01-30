ab3549a2d3612a303e5f54a37ea690abdb5cfd99
Valerie Wiklund <vkwiklund@gmail.com>
2020-01-30 10:27:06 -0700

    Router

--- wwwsrc/src/router.js
+++ wwwsrc/src/router.js
@@ -1,3 +1,21 @@
 Vue.use(Router);
 
-export default new Router({});
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/profile/userId",
+      name: "profile",
+      component: Profile
+    }
+  ]
+});
